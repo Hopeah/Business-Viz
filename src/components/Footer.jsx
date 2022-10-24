061bf880e95acc372b561f9778539ea50c54e261
Irina <irina@theluky.com>
2022-10-24 11:12:34 -0400

    Add hover effects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ function Footer() {
     return (
         <footer>
             <ul>
-                <li><a href="https://twitter.com/Hopeahh"><FaTwitterSquare size={28}/></a></li>
-                <li><a href="https://www.linkedin.com/in/irina-petrova-se/"><FaLinkedin size={28}/></a></li>
-                <li><a href="https://github.com/Hopeah" class="bi bi-github link-info fs-3 mx-3"><FaGithubSquare size={28}/></a></li>
+                <li><a href="https://twitter.com/Hopeahh" className='icon-link'><FaTwitterSquare size={28}/></a></li>
+                <li><a href="https://www.linkedin.com/in/irina-petrova-se/" className='icon-link'><FaLinkedin size={28}/></a></li>
+                <li><a href="https://github.com/Hopeah" className='icon-link'><FaGithubSquare size={28}/></a></li>
             </ul>
         </footer>
     )
