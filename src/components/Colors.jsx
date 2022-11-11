66d955f65bb9dc7407f73ee968da814d76c7e86a
Irina <irina@theluky.com>
2022-11-11 11:22:10 -0500

    Change brush placement

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -4,11 +4,11 @@ import { FaPaintBrush } from 'react-icons/fa'
 function Colors(props) {
     return (
         <div className='brushes'>
-            <span onClick={() => props.changeColor('')}><FaPaintBrush size={28} color={'pink'}/></span>
-            <span onClick={() => props.changeColor('red')}><FaPaintBrush size={28} color={'orange'}/></span>
-            <span onClick={() => props.changeColor('blue')}><FaPaintBrush size={28} color={'blue'}/></span>
-            <span onClick={() => props.changeColor('green')}><FaPaintBrush size={28} color={'green'}/></span>
-            <span onClick={() => props.changeColor('black')}><FaPaintBrush size={28}/></span>
+            <p onClick={() => props.changeColor('')}><FaPaintBrush size={28} color={'pink'}/></p>
+            <p onClick={() => props.changeColor('red')}><FaPaintBrush size={28} color={'orange'}/></p>
+            <p onClick={() => props.changeColor('blue')}><FaPaintBrush size={28} color={'blue'}/></p>
+            <p onClick={() => props.changeColor('green')}><FaPaintBrush size={28} color={'green'}/></p>
+            <p onClick={() => props.changeColor('black')}><FaPaintBrush size={28}/></p>
         </div>
     )
 }
